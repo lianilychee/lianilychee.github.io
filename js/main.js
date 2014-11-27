39e6dc75b2e96c74120bf0c13a1a4c747415407b
lianilychee <lye.liani@gmail.com>
2014-11-27 01:52:05 -0500

    hide section on click now works for both about and portfolio

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@ $(document).ready(function() {
 
 	console.log('main.js loaded');
 
+	// When clicking on "Portfolio" in nav, hide About and show Portfolio.
 	$('#portfoliolink').on('click', function() {
 		console.log('clicked portfolio!');
 		$('#about').addClass('hide');
+		$('#portfolio').removeClass('hide');
+	})
+
+	// When clicking on "About" in nav, hide Portfolio and show About.
+	$('#aboutlink').on('click', function() {
+		console.log	('clicked about!');
+		$('#about').removeClass('hide');
+		$('#portfolio').addClass('hide');
 	})
 
 
