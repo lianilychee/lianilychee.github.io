df71eb398b3f124b017af67ca279da3095c89a2e
lianilychee <lye.liani@gmail.com>
2014-11-27 01:34:33 -0500

    deleted obsolete js files; got hiding working on click!

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	console.log('main.js loaded');
+
+	$('#portfoliolink').on('click', function() {
+		console.log('clicked portfolio!');
+		$('#about').addClass('hide');
+	})
+
+
+
+})
