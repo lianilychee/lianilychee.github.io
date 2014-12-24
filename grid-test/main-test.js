826895044902ff0cba9197b7dd5a34a4322d42b9
lianilychee <lye.liani@gmail.com>
2014-12-23 22:44:32 -0800

    all projects clear selection now works

--- grid-test/main-test.js
+++ grid-test/main-test.js
@@ -1,11 +1,14 @@
 $(window).load( function() {
 
-	// When clicking on Software filter, in nav, hide About and show Portfolio.
-	$('#filter-software').on('click', function() {
-		// on items that don't have the software tag, add the class inactive
-		$('.item-container').not(".tag-software").addClass('inactive')
-
+	$('#filter-all').on('click', function() {
+		$('.item-container').removeClass('inactive')
 	})
+
+	// on items that don't have the software tag, add the class inactive
+	$('#filter-software').on('click', function() {
+		$('.item-container').not(".tag-software").addClass('inactive')
+	})
+
 
 	// When clicking on "About" in nav, hide Portfolio and show About.
 	// $('#aboutlink').on('click', function() {
