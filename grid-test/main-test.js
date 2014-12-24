ac930930625b174cd773c6f2d87b9cf8977d8d74
lianilychee <lye.liani@gmail.com>
2014-12-24 10:13:01 -0800

    fixed bug: cross selection not working

--- grid-test/main-test.js
+++ grid-test/main-test.js
@@ -6,16 +6,19 @@ $(window).load( function() {
 		$('.item-container').removeClass('inactive')
 	})
 
-	$('#filter-robo').on('click', function() {
-		$('.item-container').not(".tag-software").addClass('inactive')
+	$('#filter-design').on('click', function() {
+		$('.item-container').removeClass('inactive')
+		$('.item-container').not('.tag-design').addClass('inactive')
 	})
 
-	$('#filter-design').on('click', function() {
-		$('.item-container').not(".tag-design").addClass('inactive')
+	$('#filter-robo').on('click', function() {
+		$('.item-container').removeClass('inactive')
+		$('.item-container').not('.tag-software').addClass('inactive')
 	})
 
 	$('#filter-software').on('click', function() {
-		$('.item-container').not(".tag-software").addClass('inactive')
+		$('.item-container').removeClass('inactive')
+		$('.item-container').not('.tag-software').addClass('inactive')
 	})
 
 
