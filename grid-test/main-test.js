487cd45698dde476a00b2df6cf8ff427054259ec
lianilychee <lye.liani@gmail.com>
2014-12-24 10:54:07 -0800

    stuff

--- grid-test/main-test.js
+++ grid-test/main-test.js
@@ -1,25 +1,33 @@
 $(window).load( function() {
 
+	WIDTH = $(window).width();
+	HEIGHT = $(window).height();
+	console.log(WIDTH, HEIGHT);
+
+	GRID = $('#grid-container');
+	console.log(GRID.width(), GRID.height());
+
+	
 	// GRID FILTERING
 
 	$('#filter-all').on('click', function() {
 		$('.item-container').removeClass('inactive')
-	})
+	});
 
 	$('#filter-design').on('click', function() {
 		$('.item-container').removeClass('inactive')
 		$('.item-container').not('.tag-design').addClass('inactive')
-	})
+	});
 
 	$('#filter-robo').on('click', function() {
 		$('.item-container').removeClass('inactive')
 		$('.item-container').not('.tag-software').addClass('inactive')
-	})
+	});
 
 	$('#filter-software').on('click', function() {
 		$('.item-container').removeClass('inactive')
 		$('.item-container').not('.tag-software').addClass('inactive')
-	})
+	});
 
 
 
