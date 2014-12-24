6aae1d12ef1f8b75978e54bf253688e69835fea9
lianilychee <lye.liani@gmail.com>
2014-12-23 22:55:21 -0800

    multiple downselects implemented

--- grid-test/main-test.js
+++ grid-test/main-test.js
@@ -4,11 +4,17 @@ $(window).load( function() {
 		$('.item-container').removeClass('inactive')
 	})
 
-	// on items that don't have the software tag, add the class inactive
-	$('#filter-software').on('click', function() {
+	$('#filter-robo').on('click', function() {
 		$('.item-container').not(".tag-software").addClass('inactive')
 	})
 
+	$('#filter-design').on('click', function() {
+		$('.item-container').not(".tag-design").addClass('inactive')
+	})
+
+	$('#filter-software').on('click', function() {
+		$('.item-container').not(".tag-software").addClass('inactive')
+	})
 
 	// When clicking on "About" in nav, hide Portfolio and show About.
 	// $('#aboutlink').on('click', function() {
