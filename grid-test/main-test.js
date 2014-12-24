d0bc5f7fbc4945d832444e93ca9b164e5e7ae3a3
lianilychee <lye.liani@gmail.com>
2014-12-23 22:38:54 -0800

    downselect for single filter added! whoop whoop

--- grid-test/main-test.js
+++ grid-test/main-test.js
@@ -1,8 +1,20 @@
-$(window).load(function() {
+$(window).load( function() {
 
-	var tileCount = $('.item').length;
-	GRID.render(tileCount);
+	// When clicking on Software filter, in nav, hide About and show Portfolio.
+	$('#filter-software').on('click', function() {
+		// on items that don't have the software tag, add the class inactive
+		$('.item-container').not(".tag-software").addClass('inactive')
 
-	console.log( $('item') );
+	})
+
+	// When clicking on "About" in nav, hide Portfolio and show About.
+	// $('#aboutlink').on('click', function() {
+	// 	$('#about').removeClass('hide');
+	// 	$('#portfolio').addClass('hide');
+	// 	UTILS.resizeImg()
+	// })
+
+	// UTILS.resizeImg()
+
 
 })
