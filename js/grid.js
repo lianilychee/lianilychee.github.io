dcf2085314b4c328fe480cec2d48a3fadc54d9b5
lianilychee <lye.liani@gmail.com>
2014-12-25 11:19:22 -0800

    set up grid.js

---
+++ js/grid.js
@@ -0,0 +1,22 @@
+var GRID = {
+
+	/* Initialization. */
+	tiles: [],
+
+
+	/* Grab HTML element dimensions. */
+	onLoad: function(id) {}
+
+
+	/* Adjust grid dimensions to fit window. */
+	onResize: function() {}
+
+
+	/* Upon filter selection, update list of visible tiles. */
+	update: function() {}
+
+
+	/* Render the grid. */
+	renderGrid: function() {}
+
+}
