5f7c2515c67be015fc426fb1a0e21d5c8d6862d2
lianilychee <lye.liani@gmail.com>
2014-12-25 19:13:39 -0800

    generating a table from jquery

--- js/grid.js
+++ js/grid.js
@@ -14,24 +14,55 @@ var GRID = {
 			GRID.height = $(GRID.elementId).height();
 			// console.log(GRID.width, GRID.height);
 		});
+
+		GRID.render();
 	},
 
 
-	/* Adjust grid dimensions to fit window. */
+	/* Adjust grid cell dimensions to fit window. */
 	onResize: function() {
 		GRID.width = $(GRID.elementId).width();
 		GRID.height = $(GRID.elementId).height();
+
+		GRID.rowCount = Math.floor(GRID.height/250);
+		GRID.colCount = Math.floor(GRID.width/400);
+		// console.log(GRID.rowCount, GRID.colCount);
+
 		GRID.update();
 	},
 
 
 	/* Upon filter selection, update list of visible tiles. */
 	update: function() {
-		GRID.render();
+
+		// remove all tiles here
+
+		GRID.render();  // redraw tiles here
 	},
 
 
 	/* Render the grid. */
-	render: function() {}
+	render: function() {
+
+		// FUTZING AROUND WITH CREATING A TABLE W/ JQUERY
+
+		data = [ [1,2,3], [4,5,6], [7,8,9] ];
+
+		testTable = '<table>'
+
+		for (var i = 0; i < data.length; i++) {
+			testTable += '<tr>'; // add a rows
+			for (var j = 0, rowLen = data[i].length; j < rowLen; j++) {
+				testTable += '<td>' + data[i][j] + '</td>';
+			}
+			testTable += '</tr>';
+		}
+
+		testTable += '</table>';
+
+		$('#test-table-div').html(testTable);
+
+
+	}
 
 }
