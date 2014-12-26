3335814f774aade2ea7a39883b83a5a446846132
lianilychee <lye.liani@gmail.com>
2014-12-25 23:02:14 -0800

    moved tiles into separate js file

---
+++ js/content.js
@@ -0,0 +1,53 @@
+var CONTENT = [
+
+	/* TEMPLATE
+	item = [
+		image,
+		title,
+		blurb,
+		tags
+	], */
+
+	item1 = [
+		'image',
+		'Unclassified',
+		'blurb',
+		[]
+	],
+
+	item2 = [
+		'image',
+		'Design',
+		'blurb',
+		['tag-design']
+	],
+
+	item3 = [
+		'image',
+		'Software, Robo',
+		'blurb',
+		['tag-software', 'tag-robo']
+	],
+
+	item4 = [
+		'image',
+		'Design, Robo',
+		'blurb',
+		['tag-design', 'tag-robo']
+	],	
+
+	item5 = [
+		'image',
+		'Robo',
+		'blurb',
+		['tag-robo']
+	],
+
+	item6 = [
+		'image',
+		'Unclassified',
+		'blurb',
+		[]
+	]
+
+]
