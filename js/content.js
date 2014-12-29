e3f7ecccc35d16b1e7d381d3ef8feffbe94ddd71
lianilychee <lye.liani@gmail.com>
2014-12-28 20:56:00 -0800

    compressed thumbnail images

--- js/content.js
+++ js/content.js
@@ -4,47 +4,47 @@ var CONTENT = {
 
 	master: [	
 
-				icf = 	[	'folio/icf/thumb.jpg',
+				icf = 	[	'folio/icf/thumb.gif',
 							'ICF Prosthetics',
 							'Making below-the-elbow prostheses cheaper and easier to use.',
 							['tag-robo', 'tag-design']	],
 
-				imemio = [	'folio/imemio/thumb.jpg',
+				imemio = [	'folio/imemio/thumb.gif',
 							'imem.io',
 							'Social media analysis and a start-up experience.',
 							['tag-software', 'tag-design']	],
 
-				benji = [	'folio/benji/thumb.jpg',
+				benji = [	'folio/benji/thumb.gif',
 							'Bio-inspired Beaver',
 							'Bringing Benji to fourth-graders.',
 							['tag-design', 'tag-robo']	],
 
-				beatz = [	'folio/beatz/test.jpg',
+				beatz = [	'folio/beatz/test.gif',
 							'Beatz',
 							'OpenCV-based virtual drumset.',
 							['tag-software']	],	
 
-				ors = 	[	'folio/ors/thumb.jpg',
+				ors = 	[	'folio/ors/thumb.gif',
 							'Olin Robotic Sailing',
 							'A quest to autonomously traverse the Atlantic.',
 							['tag-robo']	],
 
-				edd = 	[	'folio/edd/thumb.jpg',
+				edd = 	[	'folio/edd/thumb.gif',
 							'Mercury',
 							'A shapeshifting, flying surveillance bot.',
 							['tag-robo']	],
 
-				kiosk = [	'folio/kiosk/thumb.jpg',
+				kiosk = [	'folio/kiosk/thumb.gif',
 							'The Food Project Kiosk',
 							'Making important information accessible.',
 							['tag-design']	],
 
-				chinese = [	'folio/chinese/test.jpg',
+				chinese = [	'folio/chinese/test.gif',
 							'Mapping the Dialects',
 							'Mapping Mandarin, Cantonese, and Hokkien to written characters.',
 							['tag-software']	],
 
-				bc = 	[	'folio/birth-control/thumb.jpg',
+				bc = 	[	'folio/birth-control/thumb-compressed.gif',
 							'The Efficacy of Birth Control',
 							'Does The Pill work for everyone?',
 							['tag-software']	]
