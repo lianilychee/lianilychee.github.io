f9aabf87df4e1d942926159025b1204a7feb2040
liani <liani@lychee.(none)>
2015-07-26 21:30:19 -0400

    scary! added grav, removed everything else

---
+++ cache/twig/2/3/23b5adefdd335ca000a59d93d4a0e6122d5ccabf11bc284fb781419ca406c5ef.php
@@ -0,0 +1,51 @@
+<?php
+
+/* default.html.twig */
+class __TwigTemplate_23b5adefdd335ca000a59d93d4a0e6122d5ccabf11bc284fb781419ca406c5ef extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("partials/base.html.twig", "default.html.twig", 1);
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "partials/base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_content($context, array $blocks = array())
+    {
+        // line 4
+        echo "\t";
+        echo $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "content", array());
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "default.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,  11 => 1,);
+    }
+}
---
+++ user/pages/03.redirect/default.md
@@ -0,0 +1,5 @@
+---
+title: Redirect
+routable: false
+---
+
