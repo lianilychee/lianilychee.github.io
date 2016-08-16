ac7b9808abeee7cea409d463b83ad57fd6688929
Liani Lye <llye@ivani.com>
2016-08-16 00:19:27 -0500

    resolved merge

---
+++ projects/project_nav.js
@@ -0,0 +1,7 @@
+document.getElementById('nav').innerHTML = 
+    "L I A N I <b>L Y E</b> // " +
+    "<a href = '../index.html'>about</a> // " +
+    "<a href = '../projects.html' " +
+        "style = 'text-decoration: none; border-bottom: 3px dotted'>" +
+        "projects</a> // " +
+    "<a href='http://www.lianilychee.wordpress.com' target='_blank'>blog</a>"
