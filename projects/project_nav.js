28a0fcd814e10158c9ac0af109ef2c63628bb4c5
Liani Lye <liani.lye@students.olin.edu>
2016-11-03 16:03:50 -0400

    standardized nav bar

--- projects/project_nav.js
+++ projects/project_nav.js
@@ -1,7 +1,7 @@
 document.getElementById('nav').innerHTML = 
     "L I A N I <b>L Y E</b> // " +
-    "<a href = '../index.html'>about</a> // " +
-    "<a href = '../projects.html' " +
+    "<a href = '../index.html' " +
         "style = 'text-decoration: none; border-bottom: 3px dotted'>" +
-        "projects</a> // " +
+    	"projects</a> // " +
+    "<a href = '../about.html'>about</a> // " +
     "<a href='../lianilye_resume_f16.pdf' target='_blank'>resume</a>"
