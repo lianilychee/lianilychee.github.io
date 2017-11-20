02ea2a461dc966af798e4bce6353209ba9431da0
Liani Lye <liani.lye@students.olin.edu>
2017-11-19 22:57:37 -0500

    File restructure and path modification

--- projects/project_nav.js
+++ projects/project_nav.js
@@ -1,13 +1,13 @@
 document.getElementById('head').innerHTML =
 	"<title>Work - Liani Lye</title>" +
-	"<link rel='stylesheet' href='../css/style.css'>" +
-    "<link rel='stylesheet' href='../css/prototype_style.css'>" +
-    "<link rel='icon' type='image/png' href='images/favicon.png'>"
+	"<link rel='stylesheet' href='../../css/style.css'>" +
+    "<link rel='stylesheet' href='../../css/prototype_style.css'>" +
+    "<link rel='icon' type='image/png' href='../../favicon.png'>"
 
 document.getElementById('nav').innerHTML = 
     "L I A N I <b>L Y E</b> // " +
-    "<a href = '../index.html' " +
+    "<a href = '../../index.html' " +
         "style = 'text-decoration: none; border-bottom: 3px dotted'>" +
     	"work</a> // " +
-    "<a href = '../about.html'>about</a> // " +
-    "<a href='../lianilye_resume_2017_07.pdf' target='_blank'>resume</a>"
+    "<a href = '../../about.html'>about</a> // " +
+    "<a href='../../lianilye_resume_2017_07.pdf' target='_blank'>resume</a>"
