a181a085c44baf9d97b3f99cc0de5a16f80c6aae
Liani Lye <lye.liani@gmail.com>
2018-07-29 21:05:32 -0700

    MOD: Changed space width from 4 -> 2

--- projects/project_nav.js
+++ projects/project_nav.js
@@ -1,13 +1,24 @@
 document.getElementById('head').innerHTML =
-	"<title>Work - Liani Lye</title>" +
-	"<link rel='stylesheet' href='../../css/style.css'>" +
+    "<title>Work - Liani Lye</title>" +
+    "<link rel='stylesheet' href='../../css/style.css'>" +
     "<link rel='stylesheet' href='../../css/prototype_style.css'>" +
     "<link rel='icon' type='image/png' href='../../img/favicon.png'>"
 
+// document.getElementById('nav').innerHTML = 
+//     "L I A N I <b>L Y E</b> // " +
+//     "<a href = '../../index.html' " +
+//         "style = 'text-decoration: none; border-bottom: 3px dotted'>" +
+//         "work</a> // " +
+//     "<a href = '../../about.html'>about</a> // " +
+//     "<a href='../../lianilye_resume_2018_07.pdf' target='_blank'>resume</a>"
+
 document.getElementById('nav').innerHTML = 
     "L I A N I <b>L Y E</b> // " +
-    "<a href = '../../index.html' " +
-        "style = 'text-decoration: none; border-bottom: 3px dotted'>" +
-    	"work</a> // " +
+
+    "<div class='dotted-container'>" +
+        "<a href = '../../index.html'> work</a>" +
+    "</div>" +
+    " // " +
+
     "<a href = '../../about.html'>about</a> // " +
     "<a href='../../lianilye_resume_2018_07.pdf' target='_blank'>resume</a>"
